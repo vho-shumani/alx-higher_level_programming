b324a1cd17f454b6ec122ee2dee3fa717fa99f87
Shumani <48450113+vho-shumani@users.noreply.github.com>
2023-11-28 19:37:12 +1100

    fixin mispelling

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -4,7 +4,7 @@ number = random.randint(-10000, 10000)
 string = str(number)
 last_digit = int(string[-1])
 if number < 0 and last_digit != 0:
-    print(f 1-last_digit.py*"Last digit of {number} is -{last_digit} and is less than 6 and not 0")
+    print(f"Last digit of {number} is -{last_digit} and is less than 6 and not 0")
 elif number > 0:
     if last_digit > 5:
         print(f"Last digit of {number} is {last_digit} and is greater than 5")
