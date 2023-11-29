685755baba05929c739578b63e93d7ad8bc22dd0
shumani <shumanisimeli@gmail.com>
2023-11-29 15:12:51 -0800

    101-remove_char_at.py

---
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def remove_char_at(str, n):
+    list = []
+    count = 0
+    for i in str:
+        list.append(i)
+    if n in range(len(str)):
+        list.remove(list[n])
+    return ' '.join(list)
