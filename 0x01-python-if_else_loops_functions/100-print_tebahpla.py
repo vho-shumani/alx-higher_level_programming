0a687b41e1e35e299e1071d55ee7f2a8b94c1ff5
shumani <shumanisimeli@gmail.com>
2023-11-29 15:21:30 -0800

    pycodestyle

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,3 +1,3 @@
 #!/usr/bin/python3
 for i in reversed(range(ord('a'), ord('z') + 1)):
-	print("{}".format(chr(i) if i % 2 == 0 else chr(i - 32)), end='')
+    print("{}".format(chr(i) if i % 2 == 0 else chr(i -32)), end='')
