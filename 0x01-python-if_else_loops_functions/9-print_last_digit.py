82230a266073bb68f126e23c43bb40765e6caec5
shumani <shumanisimeli@gmail.com>
2023-11-29 15:55:31 -0800

    fixing up pycodestyle errors

--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 def print_last_digit(number):
-	last_digit = str(number)
-	print(last_digit[-1], end='')
-	return last_digit[-1]
+    last_digit = str(number)
+    print(last_digit[-1], end='')
+    return last_digit[-1]
