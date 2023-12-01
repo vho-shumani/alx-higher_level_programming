c57f89d830298fff7d61d60e9b09661cd7969512
Shumani <48450113+vho-shumani@users.noreply.github.com>
2023-12-01 23:04:37 +0200

    task 2 to task 12

---
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def print_matrix_integer(matrix=[[]]):
+    for i in matrix:
+        for j in range(len(i)):
+            print("{}".format(i[j]), end=" " if j < len(i) - 1 else "\n")
+    
+    
---
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -0,0 +1,9 @@
+def add_tuple(tuple_a=(), tuple_b=()):
+    list1 = list(tuple_a)
+    list2 = list(tuple_b)
+    for i in range(2):
+        list1.append(0)   
+        list2.append(0)
+    tuple_c = (list1[0] + list2[0], list1[1] + list2[1])
+    return tuple_c
+
