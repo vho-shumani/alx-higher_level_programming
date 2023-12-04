6f0ff779c37f85affacd78b5fd6b967af921ba81
shumani <shumanisimeli@gmail.com>
2023-12-04 13:43:20 +0200

    update 7-add_tuple.py
    mend

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -8,10 +8,4 @@ def add_tuple(tuple_a=(), tuple_b=()):
     tuple_c = (list1[0] + list2[0], list1[1] + list2[1])
     return tuple_c
 
-tuple_a = (1, 89)
-tuple_b = (88, 11)
-new_tuple = add_tuple(tuple_a, tuple_b)
-print(new_tuple)
 
-print(add_tuple(tuple_a, (1, )))
-print(add_tuple(tuple_a, ()))
