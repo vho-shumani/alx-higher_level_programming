c2de338ef44c96cf424b60216cadd3d974477187
shumani <shumanisimeli@gmail.com>
2023-12-05 11:18:07 -0800

    100-print_python_list_info.c
    mend

--- 0x03-python-data_structures/13-main.c
+++ 0x03-python-data_structures/13-main.c
@@ -1,12 +1,21 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "lists.h"
-
 int main(void)
 {
     listint_t *head;
 
     head = NULL;
+    add_nodeint_end(&head, 1);
+    add_nodeint_end(&head, 17);
+    add_nodeint_end(&head, 972);
+    add_nodeint_end(&head, 50);
+    add_nodeint_end(&head, 98);
+    add_nodeint_end(&head, 9);
+    add_nodeint_end(&head, 50);
+    add_nodeint_end(&head, 972);
+    add_nodeint_end(&head, 17);
+    add_nodeint_end(&head, 1);
     print_listint(head);
 
     if (is_palindrome(&head) == 1)
