968d3f12dd3ac5f31f0a3876278a0a9b67bd9d8f
shumani <shumanisimeli@gmail.com>
2023-12-05 08:13:10 -0800

    13-main.c

---
+++ 0x03-python-data_structures/13-main.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+
+int main(void)
+{
+    listint_t *head;
+
+    head = NULL;
+    print_listint(head);
+
+    if (is_palindrome(&head) == 1)
+        printf("Linked list is a palindrome\n");
+    else
+        printf("Linked list is not a palindrome\n");
+
+    free_listint(head);
+
+    return (0);
+}
