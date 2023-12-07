96720dcb43fab111b32e25c976d028a62402f846
shumani <shumanisimeli@gmail.com>
2023-12-07 14:29:19 +0200

    first commit

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,7 +1,17 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
     if not matrix:
-        print("\n")
-    for i in matrix:
-        for j in range(len(i)):
-            print("{:d}".format(i[j]), end=" " if j < len(i) - 1 else "\n")
+        print(1)
+    else:
+        for i in matrix:
+            for j in range(len(i)):
+                print("{:d}".format(i[j]), end=" " if j < len(i) - 1 else "\n")
+
+matrix = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9]
+]
+
+
+print_matrix_integer()
---
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,23 @@
+def roman_to_int(roman_string):
+    roman_dic = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+    sum = 0
+    for i, x in roman_dic.items():
+        for j in roman_string:
+           if i == j: 
+                sum += x
+    return sum
+
+roman_number = "X"
+print("{} = {}".format(roman_number, roman_to_int(roman_number)))
+
+roman_number = "VII"
+print("{} = {}".format(roman_number, roman_to_int(roman_number)))
+
+roman_number = "IX"
+print("{} = {}".format(roman_number, roman_to_int(roman_number)))
+
+roman_number = "LXXXVII"
+print("{} = {}".format(roman_number, roman_to_int(roman_number)))
+
+roman_number = "DCCVII"
+print("{} = {}".format(roman_number, roman_to_int(roman_number)))
