54c0c212aefdfdd468259b9c2f6088ed86b10406
Shumani Simeli <shumanisimeli@gmail.com>
2023-12-18 15:10:45 +0200

    created 3-safe_print_division.py

---
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def safe_print_division(a, b):
+    sum = 0
+    try:
+        sum = a / b
+    except Exception:
+        sum = None
+    finally:
+        print(f'Inside result: {sum}')
+        return sum
