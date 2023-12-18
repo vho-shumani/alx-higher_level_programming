f2beb9a1f34d615ecfea9cc3f2c3bf89c1a6d95f
Shumani Simeli <shumanisimeli@gmail.com>
2023-12-18 15:13:38 +0200

    upd3-safe_print_division.py

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -6,5 +6,5 @@ def safe_print_division(a, b):
     except Exception:
         sum = None
     finally:
-        print(f'Inside result: {sum}')
+        print("Inside result: {}".format(sum))
         return sum
