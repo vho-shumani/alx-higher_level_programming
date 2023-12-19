1d96333bbfa9e521a419f765645f92fa20bfb082
Shumani Simeli <shumanisimeli@gmail.com>
2023-12-19 20:10:15 +0200

    created 3-square.py

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""define a empty class"""
+
+
+class Square:
+    """defines a square"""
+    def __init__(self, size=0):
+        """
+        define a square with given size
+
+        attributes:
+        size (int): size of the square
+        """
+        self.__size = size
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+
+    def area(self):
+        return self.__size ** 2
