cb505e69d2628728693fddce7d0ffb14b5ec907c
Shumani Simeli <shumanisimeli@gmail.com>
2024-01-08 19:58:27 +0200

    created 7-base_geometry.py

---
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+
+"""Module define BaseGeometry class"""
+
+
+class BaseGeometry:
+    """ define a square with given size
+
+        method:
+        area: raises exception.
+        integer_validator: Validates value.
+    """
+    def area(self):
+        """Raises exception with message
+
+        arg:
+        self: BaseGeometry itself.
+
+        Raise:
+        Exception: raises Exception.
+        """
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """Validates given value
+
+        args:
+        name (str): used to determine if its a string.
+        value (int): used to determine if its int
+        """
+        if not isinstance(value, int):
+            raise TypeError(f'{name} must be an integer')
+        if value <= 0:
+            raise ValueError(f'{name} must be greater than 0')
