deef616183009e7558bcc5a279f164d0a1f2265a
Shumani Simeli <shumanisimeli@gmail.com>
2024-01-08 17:57:16 +0200

    created 3-is_kind_of_class.py

---
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+
+"""Module provides is_kind_of_class function"""
+
+
+def is_kind_of_class(obj, a_class):
+    """Determines if the object is an instance of the
+    specified class and subclasses
+
+    Args:
+    obj: object to check.
+    a_class: class to check if the obj is a instance of.
+
+    Returns:
+    Boolean: True if the object is an instance
+    of the specified class and subclasses; otherwise False"""
+
+    return isinstance(obj, a_class)
