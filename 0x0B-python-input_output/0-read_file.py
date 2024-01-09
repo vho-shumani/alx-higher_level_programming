d58a06ee66664a789d6ab10e142efe798e43fabf
Shumani Simeli <shumanisimeli@gmail.com>
2024-01-09 18:17:12 +0200

    created 101-add_attribute.py
    mend

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -7,7 +7,7 @@ def read_file(filename=""):
     """reads a text file (UTF8) and prints it to stdout
 
     Parameter:
-    filename: file to read from.
+    filename (str): file to read from.
     """
-    with open(filename, 'r') as f:
+    with open(filename, 'r', encoding="utf-8" ) as f:
         print(f.read())
