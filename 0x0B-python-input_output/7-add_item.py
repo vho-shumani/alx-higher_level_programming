a33b5f638356d851fc101f3f69231acb5ca039c3
Shumani Simeli <shumanisimeli@gmail.com>
2024-01-09 23:44:24 +0200

    updated 7-add_item.py

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -13,6 +13,5 @@ try:
 except FileNotFoundError:
     mylist = [sys.argv[i] for i in range(1, len(sys.argv))]
 for i in range(1, len(sys.argv)):
-    mylist.append(sys.argv[i] )
+    mylist.append(sys.argv[i])
 save_to_json_file(mylist, "add_item.json")
-
