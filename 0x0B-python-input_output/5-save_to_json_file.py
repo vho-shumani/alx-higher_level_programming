5ac076ab1e984cc0b609082e5bf423d927484a9e
Shumani Simeli <shumanisimeli@gmail.com>
2024-01-09 19:49:57 +0200

    updated 5-save_to_json_file.py

--- 0x0B-python-input_output/5-save_to_json_file.py
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -13,6 +13,5 @@ def save_to_json_file(my_obj, filename):
         my_obj: object to be added.
         filename: file to add object.
     """
-    json_str = json.dumps(my_obj)
     with open(filename, 'w') as f:
-        f.write(json_str)
+        json.dumps(my_obj, f)
