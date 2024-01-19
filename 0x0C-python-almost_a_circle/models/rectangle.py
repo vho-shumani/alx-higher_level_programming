edea73689ff847a3cf48d95d59608503c80cf3ce
Shumani Simeli <shumanisimeli@gmail.com>
2024-01-19 14:21:17 +0200

    updated rectangle.py

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -116,7 +116,8 @@ class Rectangle(Base):
 
     def __str__(self):
         """Return a string"""
-        return f"[Rectangle] ({self.id}) {self.__x}/{self.__y} - {self.__width}/{self.__height}"
+        return (f"[Rectangle] ({self.id}) {self.__x}/{self.__y}"
+                f" - {self.__width}/{self.__height}")
 
     def update(self, *args, **kwargs):
         """Assigns an argument to each attribute:"""
