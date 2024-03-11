27e7eb6f6dc40f13638e011a1866ba61ca6d6567
vho-shumani <shumanisimeli@gmail.com>
2024-03-11 10:44:32 -0700

    5-to_integer.js

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
 const { argv } = require('node:process');
 const firstArg = parseInt(argv[2]);
-if (isNaN(firstArg)){
+if (isNaN(firstArg)) {
   console.log('Not a number');
-}else {
+} else {
   console.log(`My number: ${firstArg}`);
 }
