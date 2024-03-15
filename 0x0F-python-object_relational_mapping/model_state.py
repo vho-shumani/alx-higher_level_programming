c8c6273e63b0904abc016a49a0b988bf375bda2f
shumani <shumanisimeli@gmail.com>
2024-03-15 15:00:46 -0700

    created model_state.py

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+"""module defines a State class"""
 import sys
 from sqlalchemy import create_engine, Column, Integer, String
 from sqlalchemy.orm import declarative_base
@@ -9,6 +10,7 @@ Base = declarative_base()
 Base.metadata.create_all(engine)
 
 class State(Base):
+    """define a state"""
     __tablename__ = 'states'
     id = Column(Integer(), primary_key = True, autoincrement = True)
     name = Column(String(128), nullable=False)
