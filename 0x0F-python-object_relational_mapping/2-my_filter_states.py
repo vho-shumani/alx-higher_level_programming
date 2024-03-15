1bda1b723135bd603377a2935f8ac0ca392fc772
shumani <shumanisimeli@gmail.com>
2024-03-15 08:13:50 -0700

    updated 2-my_filter_states.py
    mend

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-""" Module takes in an argument and displays all values in the
-    states table of hbtn_0e_0_usa where name matches the argument."""
+"""displays all values in thestates table of
+   hbtn_0e_0_usa where name matches the argument."""
 import sys
 import MySQLdb
 
