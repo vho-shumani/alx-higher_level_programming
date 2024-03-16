3f4f606575d8d1a5fd5d7fbf8a41859a1c8b1219
shumani <shumanisimeli@gmail.com>
2024-03-16 08:31:58 -0700

    updated model_state.py

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -4,7 +4,7 @@ module defines a State and base class
 """
 import sys
 from sqlalchemy import create_engine, Column, Integer, String, MetaData
-from sqlalchemy.orm import declarative_base
+from sqlalchemy.ext.declarative import declarative_base
    
 metadata = MetaData()
 Base = declarative_base(metadata=mymetadata)
@@ -14,5 +14,5 @@ class State(Base):
     define a state
     """
     __tablename__ = 'states'
-    id = Column(Integer(), primary_key = True, autoincrement = True)
+    id = Column(Integer(), unique=True ,primary_key=True, autoincrement=True)
     name = Column(String(128), nullable=False)
