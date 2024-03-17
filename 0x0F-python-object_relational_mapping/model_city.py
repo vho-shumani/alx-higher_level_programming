7aab614d9c7ac3a0f0f5d742d66c6d59685944a5
shumani <shumanisimeli@gmail.com>
2024-03-17 05:56:48 -0700

    created model_city.py

---
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+Module defines a State and base class
+"""
+from sqlalchemy import Column, Integer, String, ForeignKey
+from model_state import Base
+
+
+class City(Base):
+    """
+    define cities
+    """
+    __tablename__ = 'cities'
+    id = Column(Integer(), unique=True, primary_key=True, autoincrement=True)
+    name = Column(String(128), nullable=False)
+    state_id = Column(Integer(), ForeignKey('states.id'), unique=True, nullable=False)
