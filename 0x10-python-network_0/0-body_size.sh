b48400d84c1e331f80b443cd596890189e0abc13
shumani <shumanisimeli@gmail.com>
2024-03-27 06:21:56 -0700

    updated 0-body_size.sh
    mend

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 #takes in a URL, sends a request to that URL, and displays the size of the body of the response
-results=$(curl -s -w "%{size_download}" -o /dev/null "$1")
-echo "$results"
+curl -s -w "%{size_download}" `"$1
