c46f2b0adf2a920aa5c309ff91a1326687d73480
Shumani Simeli <shumanisimeli@gmail.com>
2024-03-27 22:19:35 +0200

    updated 3-methods.sh
    mend

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # takes in a URL and displays all HTTP methods the server will accept..
-curl -sX OPTIONS "$1" | grep "Allow:" | cut -d " " -f2-
+curl -sXI "$1" | grep "Allow:" | cut -d " " -f2-
