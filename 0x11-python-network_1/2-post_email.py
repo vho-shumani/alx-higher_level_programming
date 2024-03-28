58ac4438847d5a79f533e726e6be1ec1366408da
Shumani Simeli <shumanisimeli@gmail.com>
2024-03-28 19:39:31 +0200

    created 2-post_email.py

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -4,12 +4,12 @@ with the email as a parameter, and displays the
 body of the response (decoded in utf-8)"""
 import sys
 import urllib.request
-
+import urllib.parse
 
 if __name__ == "__main__":
     url = sys.argv[1]
     email = sys.argv[2]
-    data = urllib.parse.urlencode({'email': email}).encode('uft-8')
+    data = urllib.parse.urlencode({'email': email}).encode('utf-8')
     req = urllib.request.Request(url, data)
     with urllib.request.urlopen(req) as response:
         body_response = response.read()
