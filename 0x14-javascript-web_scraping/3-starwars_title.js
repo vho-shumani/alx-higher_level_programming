2752afe32eaeca087c834c11051200cf47cd88f5
Shumani <shumanisimeli@gmail.com>
2024-04-23 22:54:44 +0200

    created 3-starwars_title.js

---
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+const request = require('request');
+
+request.get(`https://swapi-api.alx-tools.com/api/films/${process.argv[2]}`, (err, response, body) => {
+  if (err) {
+    console.error(err);
+  } else {
+    const movie = JSON.parse(body);
+    console.log(movie.title)
+  }
+}
+)
