75b6ce59d6987aec34abf9f737766cbb25b13bef
Shumani <shumanisimeli@gmail.com>
2024-04-24 01:06:34 +0200

    created 5-request_store.js

---
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -0,0 +1,16 @@
+#!/usr/bin/node
+const request = require('request');
+const fs = require('fs');
+const url = process.argv[2];
+const filePath = process.argv[3];
+
+request.get(url, (err, response, body) => {
+  if (err) {
+    console.error(err);
+  }
+  fs.writeFile(filePath, body, 'utf8', (err) => {
+    if (err) {
+      console.log(err);
+    }
+  });
+});
