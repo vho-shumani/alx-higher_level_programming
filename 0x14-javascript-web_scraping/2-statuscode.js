edbe5641952db0e5307d2a158e2162891594d9df
Shumani <shumanisimeli@gmail.com>
2024-04-23 22:24:45 +0200

    created 2-statuscode.js

---
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+const request = require('request');
+
+request.get(process.argv[2], (err, response, body) => {
+  if (err) {
+    console.error(error);
+  } else {
+    console.log(`code: ${response.statusCode}`);
+  }
+});
