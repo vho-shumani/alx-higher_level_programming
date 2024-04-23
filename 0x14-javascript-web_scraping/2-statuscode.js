66399f4a67a71c7a7759589e7bd68c2eef3e5671
Shumani <shumanisimeli@gmail.com>
2024-04-23 22:26:13 +0200

    updated 2-statuscode.js

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -3,7 +3,7 @@ const request = require('request');
 
 request.get(process.argv[2], (err, response, body) => {
   if (err) {
-    console.error(error);
+    console.error(err);
   } else {
     console.log(`code: ${response.statusCode}`);
   }
