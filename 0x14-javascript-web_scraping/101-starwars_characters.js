f42f476bf42286ba00ca0eb64c591aeeb6c25fff
Shumani <shumanisimeli@gmail.com>
2024-04-24 21:22:54 +0200

    created 101-starwars_characters.js

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -1,22 +1,18 @@
 #!/usr/bin/node
 const request = require('request');
+const requests = require('sync-request');
+
 const url = `https://swapi-api.alx-tools.com/api/films/${process.argv[2]}`;
 
 request.get(url, (err, response, body) => {
   if (err) {
     console.error(err);
   } else {
-    const movieData = JSON.parse(body);
-    const urls = movieData.characters;
-    async.mapSeries(urls, (characterUrl, callback) => {
-      request.get(characterUrl, (err, response, body) => {
-        if (err) {
-          callback(err);
-        } else {
-          const people = JSON.parse(body);
-          callback(null, people.name);
-        }
-      });
+    const films = JSON.parse(body);
+    films.characters.forEach( characterUrl => {
+	const characterRes = requests('GET', characterUrl);
+	const character = JSON.parse(characterRes.getBody());
+	console.log(character.name);
     });
   }
 });
