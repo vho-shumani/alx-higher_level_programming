74a68301872b48d10e9aad34147386fd6b92ee6e
shumani <shumanisimeli@gmail.com>
2025-06-25 19:20:39 +0200

    modified: 1-multi_languages.js

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 
-const arr = ['C is fun', 'Python is cool', 'Javascript is amazing'];
+const arr = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
 
 for (const i of arr) {
   console.log(i);
